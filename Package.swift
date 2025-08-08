24d0fdaf374b4d49f43b7e53deda6e4c81303b4a
Alexander Gorbunov <gorbunov.a@vlprojects.pro>
2025-08-08 11:22:40 +0900

    build: Add v 2.11.1 binary artefacts and package manifests

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.8
 import PackageDescription
 
-let version = "2.10.2"
-let checksum = "f2cac3542013f75c8c9f57fa33bfab464792d5cffebff3ffd1669ea58887491b"
+let version = "2.11.1"
+let checksum = "b175787dc1b7b6e946832223c3d59a21e88b5304e5f5c726ffacdbb69a6d4750"
 
 let package = Package(
 	name: "LiveDigitalSDK",
